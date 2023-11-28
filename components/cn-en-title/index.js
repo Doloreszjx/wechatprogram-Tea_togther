ce89e551bf43c55af35782505b95bc338f4fe125
doloreszjx <1173827505@qq.com>
2023-11-28 20:03:25 +0800

    add: 首页

---
+++ components/cn-en-title/index.js
@@ -0,0 +1,38 @@
+// components/cn-en-title/index.js
+Component({
+
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    // 英文名
+    enTitle: {
+      type: String,
+      value: ''
+    },
+    // 中文名
+    cnTitle: {
+      type: String,
+      value: ''
+    },
+    // 自定义样式
+    CnEnTitleCls: {
+      type: String,
+      value: ''
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
---
+++ components/common-lr-item/index.js
@@ -0,0 +1,30 @@
+// components/common-lmr-item/index.js
+Component({
+
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    item:{
+      type:Object,
+      value:{
+        title:"默认测试标题",
+        author:"yean"
+      }
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
